bb3775cbfc5f573a4fca93771e1bb7e49ab36922
TheXulis <lfgonzalez1997@gmail.com>
2017-11-12 07:43:29 +0000

    Added proper functionality for adding users

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -2,7 +2,7 @@
 
 let mongoose = require('mongoose');
 let User = mongoose.model('Users');
-
+let crypto = require('crypto');
 
 exports.list_all_users = function(req, res){
 	User.find({}, function(err, user){
@@ -15,7 +15,17 @@ exports.list_all_users = function(req, res){
 
 
 exports.create_a_user = function(req, res) {
-	let new_user = new User(req.body);
+	let salt1 = crypto.randomBytes(256).toString('hex');
+
+	//hashing password with salt
+	let hashedPassword = crypto.pbkdf2Sync(req.body.password, salt1, 100000, 256, 'sha512');
+
+	//Set up the user information
+	let info = {name:req.body.name, salt:salt1, hashedPW:hashedPassword.toString('hex')};
+	console.log(hashedPassword.toString('hex'));
+
+	//Add the user
+	let new_user = new User(info);
 	new_user.save(function(err, user){
 		if(err)
 			res.send(err);
